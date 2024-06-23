f2bd7d0e0b0aa4dbbf3878f61c89ea3745a071c7
artvsantos <arthursantos334@gmail.com>
2024-06-23 16:26:18 -0300

    Agrupando testes funcionais com pytest markers

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,9 +1,17 @@
 from selenium.webdriver.common.by import By
+import pytest
+from .base import RecipeBaseFunctionalTest
 
-from base import RecipeBaseFunctionalTest
+# Só roda os tests com a mark functional_test:
+# pytest -m 'functional_test'
+
+# roda todos os tests menos os com a mark functional_test:
+# pytest -m 'not functional_test'
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
